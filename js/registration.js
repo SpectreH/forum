5443b394a8e7950b6f0f6c217b38d4049d1653ef
SpectreH <denni.karin2803@gmail.com>
2021-11-09 15:25:17 +0200

    refactor project

---
+++ js/registration.js
@@ -0,0 +1,19 @@
+function ReturnData(nameError, emailError, username, email) {
+  if (nameError == "true" || emailError == "true") {
+    document.getElementById("username").value = username
+    document.getElementById("email").value = email
+  }
+
+  if (nameError == "true") {
+    document.getElementsByTagName("span").namedItem("name").innerHTML = "Username is already taken"
+    document.getElementsByTagName("input").namedItem("username").style.boxShadow = "0 0 6px #d45252"
+    document.getElementsByTagName("input").namedItem("username").style.borderColor = "#d45252"
+  }
+
+  if (emailError == "true") {
+    console.log(document.getElementsByClassName("error-notification").value)
+    document.getElementsByTagName("span").namedItem("email").innerHTML = "Email is already registered"
+    document.getElementsByTagName("input").namedItem("email").style.boxShadow = "0 0 6px #d45252"
+    document.getElementsByTagName("input").namedItem("email").style.borderColor = "#d45252"
+  }
+}
