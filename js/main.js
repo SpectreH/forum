f16a5922ff00fa23f67a3b2bd6ca690105847ed2
SpectreH <denni.karin2803@gmail.com>
2021-11-15 16:54:57 +0200

    implement add new category function, fix alert messages with mass click

--- js/main.js
+++ js/main.js
@@ -42,21 +42,6 @@ function CreateLogOutButton() {
   headerListElement.appendChild(li)
 }
 
-function GenerateAlertBox(messageType, alertMessage) {
-  if (messageType != "") {
-    element = document.getElementsByTagName("div").namedItem("alert");
-    document.getElementsByTagName("h1").namedItem("alert-text").innerHTML = alertMessage;
-    element.style.display = "flex";
-
-    if (messageType != "Login" && messageType  != "Register" && messageType  != "Logout") {
-      document.getElementsByTagName("h1").namedItem("alert-text").style.backgroundColor = "rgba(211, 10, 10, 0.85)";
-      document.getElementsByTagName("h1").namedItem("alert-text").style.border = "2px solid rgba(211, 10, 10, 0.85)";
-    }
-
-    setTimeout(function () { AlerFadeOut(element); }, 3000);
-  }
-}
-
 function SetCategories() {
   var categoriesSectionsLenght = document.getElementsByClassName("post-entry-categories").length;
   var categoriesSections = document.querySelectorAll("div.post-entry-categories");
@@ -83,6 +68,7 @@ function CreateShowMoreCatButton(element, lenght) {
   div.classList.add("category-box");
   div.setAttribute('type', "show-more");
 
+  a.classList.add("category-box-content");
   a.setAttribute('type', "category");
   a.setAttribute('rel', "nofollow");
   a.setAttribute('onclick', "ShowAllCategories(this)");
@@ -102,12 +88,44 @@ function ShowAllCategories(element) {
   element.parentElement.remove();
 }
 
+
+let ALERT_TIMER;
+let TIME_OUT;
+function GenerateAlertBox(messageType, alertMessage) {
+  element = document.getElementsByTagName("div").namedItem("alert");
+
+  if (element.style.display == "flex") {
+    clearInterval(ALERT_TIMER);
+    clearTimeout(TIME_OUT);
+  }
+
+  if (messageType != "") {
+    element.style.opacity = 1;
+    element = document.getElementsByTagName("div").namedItem("alert");
+    document.getElementsByTagName("h1").namedItem("alert-text").innerHTML = alertMessage;
+    element.style.display = "flex";
+
+    if (messageType != "Login" && messageType != "Register" && messageType != "Logout") {
+      document.getElementsByTagName("h1").namedItem("alert-text").style.backgroundColor = "rgba(211, 10, 10, 0.85)";
+      document.getElementsByTagName("h1").namedItem("alert-text").style.border = "2px solid rgba(211, 10, 10, 0.85)";
+    }
+
+    TIME_OUT = setTimeout(function () { AlerFadeOut(element); }, 3000);
+  }
+}
+
+function ClearAlertMessage(element) {
+  clearInterval(ALERT_TIMER);
+  clearTimeout(TIME_OUT);
+  element.style.display = "none";
+}
+
 function AlerFadeOut(element) {
   var op = 1;  // initial opacity
-  var timer = setInterval(function () {
+  ALERT_TIMER = setInterval(function () {
 
     if (op <= 0.01) {
-      clearInterval(timer);
+      clearInterval(ALERT_TIMER);
       element.style.display = "none";
     }
     element.style.opacity = op;
--- js/new.js
+++ js/new.js
@@ -1,12 +1,95 @@
+let ALL_CATEGORIES = [];
+
 function CheckFileSize(element) {
   if (element.files.length > 0) {
     const fileSize = element.files.item(0).size;
     if (fileSize < 2048000) {
       document.getElementById("imageName").innerHTML = element.files.item(0).name
     } else {
-      alert("File too big, please select a file less than 2 mb");
+      GenerateAlertBox("FileSize", "File too big, please select a file less than 2 mb");
       element.value = null;
       document.getElementById("imageName").innerHTML = ""
     }
   }
+}
+
+function SubmitForm() {
+  var mainForm = document.getElementById("mainForm");
+  var categorieForm = document.getElementById("allNewCategories");
+
+  for (i = 0; i < mainForm.length; i++) {
+    if (mainForm[i].checkValidity() == false) {
+      mainForm[i].reportValidity();
+      return
+    }
+  }
+
+  if (categorieForm.childElementCount < 1) {
+    GenerateAlertBox("CategoryCounter", "Require at least 1 category!");
+    return
+  }
+
+  mainForm.submit();
+}
+
+function DeleteNewCategorie(element) {
+  for (i = 0; i < ALL_CATEGORIES.length; i++) {
+    if (element.getAttribute("value") == ALL_CATEGORIES[i]) {
+      ALL_CATEGORIES.splice(i);
+    }
+  }
+
+  element.parentElement.remove();
+}
+
+function AddCategorie() {
+  input = document.getElementById("newCategorie");
+
+  if (input.value.length < 3) {
+    GenerateAlertBox("CategorySize", "New category length must be at least 3 character long!");
+    return;
+  }
+  
+  if (CheckCategoryDublicates(input.value)) {
+    GenerateAlertBox("CategoryDublicate", "You are already added this category!");
+    return;
+  }
+
+  ALL_CATEGORIES.push(input.value);
+
+  var div = document.createElement("div");
+  var a = document.createElement("a");
+  var span = document.createElement("span");
+  var node = document.createTextNode(input.value);
+
+  div.classList.add("category-box");
+
+  a.classList.add("category-box-content");
+  a.setAttribute('type', "category");
+  a.setAttribute('rel', "nofollow");
+  a.setAttribute('href', "#");
+  a.setAttribute('value', input.value);
+  a.setAttribute('onclick', "DeleteNewCategorie(this)");
+
+  span.classList.add("close-categorie");
+
+  span.appendChild(document.createTextNode("X"));
+  a.appendChild(node);
+
+  a.appendChild(span);
+  div.appendChild(a);
+
+  var parentElement = document.getElementById("allNewCategories");
+  parentElement.appendChild(div);
+
+  input.value = "";
+}
+
+function CheckCategoryDublicates(categorieName) {
+  for (i = 0; i < ALL_CATEGORIES.length; i++) {
+    if (categorieName == ALL_CATEGORIES[i]) {
+      return true;
+    }
+  }
+  return false;
 }
